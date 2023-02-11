d057b3c6ca6caf7be23bf5e744ba8162b35ffdba
Alex Staelens <Alex.Staelens@vu.com>
2023-02-11 16:10:50 -0600

    added while statement to calculate compound interest 3 times

--- compoundInterest.py
+++ compoundInterest.py
@@ -2,10 +2,20 @@ def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundInterest()
 # This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+ n=0
+ while n <3:
+    client_one_principal = float(input("Principle (amount): "))
+    client_one_time =      float(input("Time:               "))
+    client_one_rate =      float(input("Rate:               "))
+    
+    amount=client_one_principal*(1+client_one_rate/100)**client_one_time
+    intrest=amount-client_one_principal
+    intrest_100=intrest*100
+    intrest=round(intrest_100)/100
+    print("Compound Interest: "+str(intrest))
+    n=n+1
+    if n<3:
+        print("---")
 
     # end assignment
 
